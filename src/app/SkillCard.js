19ce05bdf8c011a43c4464c95c9c2537ebe37fed
khadija-mahdi <khadijamahdi6@gmail.com>
2024-10-18 15:26:32 +0100

    "add skills"

--- src/app/SkillCard.js
+++ src/app/SkillCard.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const SkillCard = ({ icon, name }) => {
   return (
-<div className="flex flex-col items-center border rounded-lg p-4 hover:bg-gray-100 transition min-h-[186px] min-w-[186px]">
+<div className="flex flex-col items-center justify-center gap-4 border rounded-lg border-black  hover:bg-gray-100 transition lg:h-[180px] lg:w-[180px] max-h-[180px] max-w-[180px] min-h-[100px] min-w-[100px]">
       <div className="mb-2">{icon}</div>
-      <div className="text-center">{name}</div>
+      <div className="text-center font-bold">{name}</div>
     </div>
   );
 };
